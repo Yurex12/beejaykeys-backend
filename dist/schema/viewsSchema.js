4aa3f06bade73330afbf314b9c9f348cb3190c43
Adeyemi <yusufekungomi@gmail.com>
2026-02-16 11:37:13 +0100

    Delete dist directory

--- dist/schema/viewsSchema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.viewsSchema = void 0;
-const zod_1 = require("zod");
-exports.viewsSchema = zod_1.z.object({
-    ipAddress: zod_1.z.string(),
-});
